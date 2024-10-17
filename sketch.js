27ec932c55cc87332584eba4be4ab3d9e27f0beb
Kangcroline <161738160+kangcaroline@users.noreply.github.com>
2024-10-17 08:43:40 -0300

    alteraçoes

--- sketch.js
+++ sketch.js
@@ -7,24 +7,45 @@
 //adoreveis mulheres, 10 anos,drama
 //uma linda mulher,12 anos,romance
 
-function setup(){ 
-  createCanvas(600, 500);
-}
 
+let campoIdade;
+let campoFantasia;
+
+function setup() {
+    createCanvas(600, 600);
+   createSpan("Sua idade:");
+    campoIdade = createInput("15");
+    campoFantasia = createCheckbox("Gosta de Drama ?");
+}
 function draw() {
-  background(220);
+    background("white");
+    let idade = campoIdade.value();
+    let gostaDeFantasia = campoFantasia.checked();
+    let gostaDeAventura = campoAventura.checked();
+    let recomendacao = geraRecomendacao(idade, gostaDeFantasia, gostaDeAventura);
   
-  let recomendacao = geraRecomendacao();
-  text (recomendacao, width / 2, height /2);
-}
-
-function geraRecomendacao() {
-   if(idade >= 16) {
-    return "o diario da princesa";
-  } else {
-    
-    return "parasita";
+    fill(color(76, 0, 115));
+    textAlign(CENTER, CENTER);
+    textSize(38);
+    text(recomendacao, width / 2, height / 2);
   }
-  
-} 
 
+  function geraRecomendacao(idade, gostaDeFantasia) {
+    if(idade >= 16) {
+        if(idade >= 12) {
+            return "Vingadores Ultimato";
+        } else {
+            if(gostaDeDrama){
+                return "o diario da princesa";
+            } else {
+                return "Parasita";
+            }
+        }
+    } else {
+ if (gostaDeRomance) {
+            return "uma linda mulher";
+        } else {
+            return "O diario da princesa";
+        }
+    }
+}
